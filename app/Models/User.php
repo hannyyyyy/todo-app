8f111b2cbacc012c88f6e3880f4525bc81efdf12
hanny <hannyarsysyafaat@gmail.com>
2025-02-17 10:08:23 +0700

    menambahkan komentar

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+// Menggunakan fitur autentikasi bawaan Laravel
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,10 +11,12 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable; // Menggunakan trait HasFactory untuk pembuatan data dummy dan Notifiable untuk notifikasi
+
 
     /**
-     * The attributes that are mass assignable.
+     * Atribut yang dapat diisi secara massal.
+     * Atribut ini memungkinkan data diisi dengan metode create() atau update().
      *
      * @var list<string>
      */
