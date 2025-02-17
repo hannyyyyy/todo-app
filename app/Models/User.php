d4d3410c34069d4c2af168a9a8716193956a41ce
hanny <hannyarsysyafaat@gmail.com>
2025-02-17 10:20:39 +0700

    menambahkan komentar

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,8 +46,8 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime', 
-            'password' => 'hashed',
+            'email_verified_at' => 'datetime', // Mengubah nilai ke format DateTime saat diambil dari database
+            'password' => 'hashed',  // Menggunakan hashing untuk menyimpan password dengan aman
         ];
     }
 }
