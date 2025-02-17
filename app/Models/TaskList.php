ddace8c2ce51290a5c70523221cddec1fcbc2448
hanny <hannyarsysyafaat@gmail.com>
2025-02-17 09:46:52 +0700

    menambahkan komentar

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TaskList extends Model
 {
     // Menentukan atribut yang dapat diisi secara massal
-    protected $fillable = ['name'];     
+    protected $fillable = ['name'];  // Nama daftar tugas (Task List) 
     protected $guarded = [
         'id',
         'created_at',
