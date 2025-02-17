2d0bab51964416cf5efa5f65e9b5161b5a2d4821
hanny <hannyarsysyafaat@gmail.com>
2025-02-17 09:48:41 +0700

    menambahkan komentar

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -8,6 +8,8 @@ class TaskList extends Model
 {
     // Menentukan atribut yang dapat diisi secara massal
     protected $fillable = ['name'];  // Nama daftar tugas (Task List) 
+
+    // Menentukan atribut yang tidak boleh diisi secara massal
     protected $guarded = [
         'id',
         'created_at',
