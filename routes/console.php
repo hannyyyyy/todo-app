8425eca60873634fbc1968dd2e65340c710ae80d
hanny <hannyarsysyafaat@gmail.com>
2025-02-20 08:51:21 +0700

    menambahkan komen

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,14 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Mendefinisikan perintah Artisan kustom dengan nama 'inspire'
 Artisan::command('inspire', function () {
+    // Menampilkan kutipan inspiratif dari kelas Inspiring
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})
+
+// Menetapkan tujuan dari perintah ini, yaitu untuk menampilkan kutipan inspiratif
+->purpose('Display an inspiring quote')
+
+// Menjadwalkan agar perintah ini dijalankan setiap jam
+->hourly();
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,24 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
 
-// Membuat route untuk home
+// Membuat route untuk halaman utama (home), yang akan memanggil metode 'index' dari TaskController
 Route::get('/', [TaskController::class, 'index'])->name('home');
 
+// Membuat resource route untuk TaskListController
+// Route ini secara otomatis akan membuat semua metode CRUD (index, create, store, show, edit, update, destroy)
 Route::resource('lists', TaskListController::class);
 
+// Membuat resource route untuk TaskController
+// Sama seperti sebelumnya, ini akan membuat route untuk operasi CRUD pada tasks
 Route::resource('tasks', TaskController::class);
+
+// Route khusus untuk menandai sebuah tugas sebagai selesai
+// Menggunakan metode PATCH karena hanya memperbarui sebagian dari data tugas
 Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
 
+// Route untuk memindahkan tugas ke daftar (list) lain
+// Menggunakan metode PATCH karena hanya mengubah atribut tertentu dalam tugas
 Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
 
+// Route untuk halaman "About", yang akan memanggil metode 'about' di TaskController
 Route::get('/about', [TaskController::class, 'about'])->name('about');
